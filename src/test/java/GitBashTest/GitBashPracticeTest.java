03c9659ce51ef73c31ec6cf27579c587a6fafaa6
Santosh <santoshmasti419@gmail.com>
2022-06-01 12:24:06 +0530

    GitBash Update

---
+++ src/test/java/GitBashTest/GitBashPracticeTest.java
@@ -0,0 +1,13 @@
+package GitBashTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class GitBashPracticeTest {
+	@Test
+	public void gitBashPractice1()
+	{
+		Reporter.log("hi Dany",true);
+	}
+
+}
